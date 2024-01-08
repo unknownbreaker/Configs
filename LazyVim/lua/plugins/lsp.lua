2fff471ec72db46dedc23e82116f23abf031435f
Rob Yang <rob.yang@flightaware.com>
2024-01-08 20:28:24 +0000

    Fix lsp stuff

--- LazyVim/lua/plugins/lsp.lua
+++ LazyVim/lua/plugins/lsp.lua
@@ -1,5 +1,54 @@
 return {
 
+  -- Add more LSPs
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        cssls = {},
+        docker_compose_language_service = {},
+        dockerls = {},
+        emmet_ls = {},
+        gopls = {},
+        graphql = {},
+        html = {},
+        jsonls = {},
+        ruff_lsp = {},
+        rust_analyzer = {},
+        sqlls = {},
+        tailwindcss = {},
+        tsserver = {},
+        volar = {},
+        yamlls = {},
+      },
+    },
+  },
+
+  -- Colors
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      user_default_options = {
+        tailwind = true,
+      },
+    },
+  },
+
+  -- Include color swatches in auto-complete menu
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "roobert/tailwindcss-colorizer-cmp.nvim", config = true
+    },
+    opts = function(_, opts)
+      local format_kinds = opts.formatting.format
+      opts.formatting.format = function(entry, item)
+        format_kinds(entry, item)
+        return require("tailwindcss-colorizer-cmp").formatter(entry, item)
+      end
+    end,
+  },
+
 	-- Deno plugin
 	{
 		"sigmaSd/deno-nvim",
@@ -7,7 +56,10 @@ return {
 	},
 
 	-- Lua plugin
-	{ "folke/neodev.nvim", opts = {} },
+	{ "folke/neodev.nvim",
+    dependencies = { "neovim/nvim-lspconfig" },
+    opts = {},
+  },
 
 	-- TypeScript plugin
 	{
