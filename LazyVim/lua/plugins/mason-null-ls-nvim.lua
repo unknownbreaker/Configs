16c798084aa265b74ee5c77c6668b6575a254afc
Rob Yang <rob.yang@flightaware.com>
2024-01-25 17:26:48 +0000

    Fix indents

--- LazyVim/lua/plugins/mason-null-ls-nvim.lua
+++ LazyVim/lua/plugins/mason-null-ls-nvim.lua
@@ -1,50 +1,50 @@
 return {
-	"jay-babu/mason-null-ls.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"williamboman/mason.nvim",
-		"nvimtools/none-ls.nvim",
-	},
-	config = function()
-		require("mason-null-ls").setup({
-			ensure_installed = {
-				-- Formatters
-				"black", -- Python
-				"isort", -- Python
-				"fish_indent",
-				"flake8",
-				"goimports",
-				"gofumpt",
-				"jq",
-				"prettierd",
-				"shfmt",
-				"sql-formatter",
-				"stylua",
+  "jay-babu/mason-null-ls.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "williamboman/mason.nvim",
+    "nvimtools/none-ls.nvim",
+  },
+  config = function()
+    require("mason-null-ls").setup({
+      ensure_installed = {
+        -- Formatters
+        "black", -- Python
+        "isort", -- Python
+        "fish_indent",
+        "flake8",
+        "goimports",
+        "gofumpt",
+        "jq",
+        "prettierd",
+        "shfmt",
+        "sql-formatter",
+        "stylua",
 
-				-- Linters
-				"eslint_d",
-				"shellcheck",
-				"sqlfluff",
+        -- Linters
+        "eslint_d",
+        "shellcheck",
+        "sqlfluff",
 
-				-- LSPs
-				"css-lsp",
-				"docker-compose-language-service",
-				"dockerfile-language-server",
-				"emmet-ls",
-				"gopls",
-				"graphql-language-service-cli",
-				"html-lsp",
-				"json-lsp",
-				"ruff-lsp", -- python
-				"rust-analyzer",
-				"sqlls",
-				"tailwindcss-language-server",
-				-- "typescript-language-server",
-				"vue-language-server",
-				"yaml-language-server",
-			},
-			automatic_installation = false,
-			handlers = {},
-		})
-	end,
+        -- LSPs
+        "css-lsp",
+        "docker-compose-language-service",
+        "dockerfile-language-server",
+        "emmet-ls",
+        "gopls",
+        "graphql-language-service-cli",
+        "html-lsp",
+        "json-lsp",
+        "ruff-lsp", -- python
+        "rust-analyzer",
+        "sqlls",
+        "tailwindcss-language-server",
+        -- "typescript-language-server",
+        "vue-language-server",
+        "yaml-language-server",
+      },
+      automatic_installation = false,
+      handlers = {},
+    })
+  end,
 }
