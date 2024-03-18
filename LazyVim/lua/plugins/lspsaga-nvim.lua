89a082eb55a3ff3ce20abed910d19b4e8e6e79bb
Rob Yang <rob.yang@flightaware.com>
2024-03-18 15:22:51 +0000

    Setup lsp saga

---
+++ LazyVim/lua/plugins/lspsaga-nvim.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    "nvimdev/lspsaga.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons", "neovim/nvim-lspconfig" },
+    enabled = true,
+  },
+}
