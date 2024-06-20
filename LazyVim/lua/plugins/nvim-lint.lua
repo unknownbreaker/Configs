0dfabb162f3c2e074f8c3368c00ee2b7cc4de909
ryang <rob.yang@flightaware.com>
2024-06-20 10:23:21 -0400

    Remove redundant languages

--- LazyVim/lua/plugins/nvim-lint.lua
+++ LazyVim/lua/plugins/nvim-lint.lua
@@ -8,14 +8,9 @@ return {
     opts = {
       linters_by_ft = {
         go = { "golangcit_lint" },
-        -- javascript = { "eslint_d" },
-        -- javascriptreact = { "eslint_d" },
         sh = { "shellcheck" },
         sql = { "sqlfluff" },
         tcl = { "tcl" },
-        -- typescript = { "eslint_d" },
-        -- typescriptreact = { "eslint_d" },
-        -- vue = { "eslint_d" },
       },
     },
   },
