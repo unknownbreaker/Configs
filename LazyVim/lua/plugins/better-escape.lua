c646f2ab0c15cc86ee5ce244b04644a57258916d
ryang <rob.yang@flightaware.com>
2024-07-09 12:07:47 -0400

    Add better escape plugin

---
+++ LazyVim/lua/plugins/better-escape.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "max397574/better-escape.nvim",
+    config = function()
+      require("better_escape").setup({
+        mappings = {
+          i = {
+            [" "] = {
+              ["<tab>"] = function()
+                -- Defer execution to avoid side-effects
+                vim.defer_fn(function()
+                  -- set undo point
+                  vim.o.ul = vim.o.ul
+                  require("luasnip").expand_or_jump()
+                end, 1)
+              end,
+            },
+            j = {
+              k = false,
+              j = false,
+            },
+          },
+        },
+      })
+    end,
+  },
+}
