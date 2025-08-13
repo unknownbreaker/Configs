caadd8359487adfcf8b7dae0f6fa07006c896717
ryang <rob.yang@flightaware.com>
2025-08-13 12:21:22 -0400

    Simplify nvim-lint config

--- LazyVim/lua/plugins/nvim-lint.lua
+++ LazyVim/lua/plugins/nvim-lint.lua
@@ -1,23 +1,10 @@
 return {
   "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local lint = require("lint")
-
-    lint.linters_by_ft = {
+  opts = {
+    events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+    linters_by_ft = {
       tcl = { "nagelfar" },
-    }
-
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-      group = lint_augroup,
-      callback = function()
-        lint.try_lint()
-      end,
-    })
-
-    vim.keymap.set("n", "<leader>cl", function()
-      lint.try_lint()
-    end, { desc = "Trigger linting for current file" })
-  end,
+      rvt = { "nagelfar" },
+    },
+  },
 }
