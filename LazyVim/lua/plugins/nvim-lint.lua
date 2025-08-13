18e30147610c2f0aeea09f6028e3504ce84535d1
ryang <rob.yang@flightaware.com>
2025-08-13 11:48:12 -0400

    Use simplified nagelfar lint setup

--- LazyVim/lua/plugins/nvim-lint.lua
+++ LazyVim/lua/plugins/nvim-lint.lua
@@ -1,51 +1,23 @@
 return {
-  -- nvim-lint
-  {
-    "mfussenegger/nvim-lint",
-    ft = { "tcl", "rvt" },
-    config = function()
-      require("lint").linters_by_ft = {
-        tcl = { "nagelfar" },
-        rvt = { "nagelfar" }, -- For your Rivet files
-      }
+  "mfussenegger/nvim-lint",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local lint = require("lint")
 
-      -- Custom Nagelfar linter configuration
-      require("lint").linters.nagelfar = {
-        cmd = "nagelfar.tcl",
-        stdin = false,
-        args = {},
-        stream = "stdout",
-        ignore_exitcode = true,
-        parser = function(output, bufnr)
-          local diagnostics = {}
-          for line in output:gmatch("[^\r\n]+") do
-            -- Parse Nagelfar output: "Line   5: W Found constant..."
-            local line_num, severity, message = line:match("^Line%s+(%d+):%s+([NEW])%s+(.+)$")
-            if line_num then
-              local diagnostic_severity = severity == "E" and vim.diagnostic.severity.ERROR
-                  or severity == "W" and vim.diagnostic.severity.WARN
-                  or vim.diagnostic.severity.INFO
+    lint.linters_by_ft = {
+      tcl = { "nagelfar" },
+    }
 
-              table.insert(diagnostics, {
-                lnum = tonumber(line_num) - 1,
-                col = 0,
-                message = message,
-                severity = diagnostic_severity,
-                source = "nagelfar",
-              })
-            end
-          end
-          return diagnostics
-        end,
-      }
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      group = lint_augroup,
+      callback = function()
+        lint.try_lint()
+      end,
+    })
 
-      -- Auto-lint on save
-      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-        pattern = { "*.tcl", "*.rvt" },
-        callback = function()
-          require("lint").try_lint()
-        end,
-      })
-    end,
-  },
+    vim.keymap.set("n", "<leader>cl", function()
+      lint.try_lint()
+    end, { desc = "Trigger linting for current file" })
+  end,
 }
